ffb1c36f42c79175f2bcd1f9bde30c081817d6f2
cheoneunjeong <0102skyjung@naver.com>
2024-06-03 09:42:13 +0900

    FrontController 도입
    프론트 컨트롤러에서 HttpServlet 확장 후, 해당 요청 uri에 맞는 컨트롤러를 호출하는 방식

---
+++ src/main/java/hello/servlet/web/frontController/v1/FrontControllerServletV1.java
@@ -0,0 +1,35 @@
+package hello.servlet.web.frontController.v1;
+
+import hello.servlet.web.frontController.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontController.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontController.v1.controller.MemberSaveControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private HashMap<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ControllerV1 controllerV1 = controllerMap.get(req.getRequestURI());
+        if (controllerV1 == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        controllerV1.process(req, resp);
+    }
+}
--- src/main/java/hello/servlet/web/frontController/v1/controller/MemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/frontController/v1/controller/MemberFormControllerV1.java
@@ -1,19 +1,17 @@
-package hello.servlet.web.servletmvc;
+package hello.servlet.web.frontController.v1.controller;
 
+import hello.servlet.web.frontController.v1.ControllerV1;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
-public class MvcMemberFormServlet extends HttpServlet {
+public class MemberFormControllerV1 implements ControllerV1 {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
         dispatcher.forward(req, resp);
